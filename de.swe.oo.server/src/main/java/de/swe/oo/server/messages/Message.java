6a67dcb263c8f67fa0827b7a08bd24d01224c07e
Yixue.Dai <dai@cip.ifi.lmu.de>
2022-05-04 15:28:13 +0200

    delete the enum class "Message Type" and as well as where it was used in other classes

--- de.swe.oo.server/src/main/java/de/swe/oo/server/messages/Message.java
+++ de.swe.oo.server/src/main/java/de/swe/oo/server/messages/Message.java
@@ -4,7 +4,6 @@ package de.swe.oo.server.messages;
 import de.swe.oo.server.player.Player;
 
 abstract public class Message {
-    MessageType type;
     String messageText;
 
     abstract public void handle(Player player);
