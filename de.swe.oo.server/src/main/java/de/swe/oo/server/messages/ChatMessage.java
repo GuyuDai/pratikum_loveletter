aa4206d1bb7b4b18b5d70405fa35a3cf07b591cb
Yixue.Dai <dai@cip.ifi.lmu.de>
2022-05-13 00:01:33 +0200

    send messages client to client

--- de.swe.oo.server/src/main/java/de/swe/oo/server/messages/ChatMessage.java
+++ de.swe.oo.server/src/main/java/de/swe/oo/server/messages/ChatMessage.java
@@ -14,9 +14,30 @@ public class ChatMessage extends Message {
             player.quit();
             return;
         }
-        ChatMessage messageWithSender =
-                new ChatMessage(player.name + ": " + messageText);
-        player.session.broadcast(messageWithSender);
+
+        if(messageText.contains("@")){
+            int begin = messageText.lastIndexOf("@") + 1;
+            int end = messageText.length() - 1;
+            while(begin != end){
+                String name = messageText.subSequence(begin, end).toString();
+                for(String player_name : player.session.getPlayers()){
+                    if(name.equals(player_name)){
+                        ChatMessage messageWithSender =
+                            new ChatMessage(player.name + ": " + messageText);
+                        player.session.sendTo(messageWithSender, name);
+                        return;
+                    }
+                }
+                end --;
+            }
+            player.session.sendTo
+                (new ChatMessage("the @person is not in the game"), player.name);
+
+        }else{
+            ChatMessage messageWithSender =
+            new ChatMessage(player.name + ": " + messageText);
+            player.session.broadcast(messageWithSender);
+        }
     }
 
     public String output() {
--- de.swe.oo.server/src/main/java/de/swe/oo/server/session/Session.java
+++ de.swe.oo.server/src/main/java/de/swe/oo/server/session/Session.java
@@ -4,6 +4,7 @@ import de.swe.oo.server.messages.ChatMessage;
 import de.swe.oo.server.messages.Message;
 import de.swe.oo.server.player.Player;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -45,6 +46,22 @@ public class Session extends Thread {
         for (Player player : players) {
             player.sendMessage(msg);
         }
+    }
+
+    public void sendTo(Message msg, String target_name){
+        for (Player player : players) {
+            if(player.name.equals(target_name)){
+                player.sendMessage(msg);
+            }
+        }
+    }
+
+    public List<String> getPlayers(){
+        List<String> players_name = new ArrayList<>();
+        for(Player player : players){
+            players_name.add(player.name);
+        }
+        return players_name;
     }
 
     public static void main(String[] args) {
