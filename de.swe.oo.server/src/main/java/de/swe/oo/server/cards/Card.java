0d26258f5ddd2aeaf6b081d01ed1b8c8b3522635
Nik <nissln@gitlab2.cip.ifi.lmu.de>
2022-05-16 14:22:41 +0200

    Merged with Dai and did Princess discard method

--- de.swe.oo.server/src/main/java/de/swe/oo/server/cards/Card.java
+++ de.swe.oo.server/src/main/java/de/swe/oo/server/cards/Card.java
@@ -1,16 +1,17 @@
 package de.swe.oo.server.cards;
 
+import de.swe.oo.server.game.LoveLetterGame;
 import de.swe.oo.server.player.Player;
 import de.swe.oo.server.game.Game;
 
 public abstract class Card {
     protected String name;
     protected int value;
-    protected Game currentGame;
+    protected LoveLetterGame currentGame;
     protected Player owner;
 
 
-    public Card(String name, int value, Game currentGame, Player owner) {
+    public Card(String name, int value, LoveLetterGame currentGame, Player owner) {
         this.name = name;
         this.value = value;
         this.currentGame = currentGame;
--- de.swe.oo.server/src/main/java/de/swe/oo/server/cards/Princess.java
+++ de.swe.oo.server/src/main/java/de/swe/oo/server/cards/Princess.java
@@ -1,18 +1,19 @@
 package de.swe.oo.server.cards;
 
 import de.swe.oo.server.game.Game;
+import de.swe.oo.server.game.LoveLetterGame;
 import de.swe.oo.server.player.Player;
 
 public class Princess extends Card {
     private static String NAME = "Princess";
     private static int VALUE = 8;
 
-    public Princess(Game currentGame, Player owner) {
+    public Princess(LoveLetterGame currentGame, Player owner ) {
         super(NAME, VALUE, currentGame, owner);
     }
     /** If the player plays or discards this card for any reason, they are eliminated from the round*/
     @Override
     void discard(){
-
+    this.currentGame.playerKickedOff(owner);
     }
 }
