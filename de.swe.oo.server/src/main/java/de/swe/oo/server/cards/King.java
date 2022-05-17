6658580c7fb0441134774d639e1bf72e9b3c7076
Nik <nissln@gitlab2.cip.ifi.lmu.de>
2022-05-17 18:19:57 +0200

    King Method

--- de.swe.oo.server/src/main/java/de/swe/oo/server/cards/King.java
+++ de.swe.oo.server/src/main/java/de/swe/oo/server/cards/King.java
@@ -2,6 +2,8 @@ package de.swe.oo.server.cards;
 
 import de.swe.oo.server.game.Game;
 import de.swe.oo.server.game.LoveLetterGame;
+import de.swe.oo.server.messages.GameChoiceRequestMessage;
+import de.swe.oo.server.messages.GameMessage;
 import de.swe.oo.server.player.Player;
 
 public class King extends Card {
@@ -15,6 +17,23 @@ public class King extends Card {
     //Player may trade hands with other player
     @Override
     void discard(){
-
+        /** create nameList with all the Names of active Players */
+        String [] namelist = currentGame.getNameOfActivePlayers().toArray(new String[0]);
+        /** Player can choose a name to show their hands*/
+        owner.sendMessage(new GameChoiceRequestMessage("Choose one of the names to trade decks", namelist));
+        /** Chosen player needs to show his/her cards*/
+        Player targetPlayer= currentGame.getPlayer(owner.getLastResponse());
+        owner.sendMessage(new GameMessage(currentGame.choosePlayerDeck(targetPlayer)));
+        owner.sendMessage(new GameMessage("Do you want to trade decks?"));
+        /** If yes decks get changed*/
+        if (owner.getLastResponse() == "yes" ||owner.getLastResponse()=="Yes"){
+            /** card1 and card 2 as temporary safed cards */
+            Card card1= targetPlayer.getHands(0);
+            Card card2= targetPlayer.getHands(1);
+            targetPlayer.setHands(owner.getHands(0));
+            targetPlayer.setHands(owner.getHands(1));
+            owner.setHands(card1);
+            owner.setHands(card2);
+        }
     }
 }
