0e77d9b8b1ebc9be65b3d3ffef3cd029c6165574
Nik <nissln@gitlab2.cip.ifi.lmu.de>
2022-05-17 17:10:40 +0200

    Guard Method

--- de.swe.oo.server/src/main/java/de/swe/oo/server/cards/Guard.java
+++ de.swe.oo.server/src/main/java/de/swe/oo/server/cards/Guard.java
@@ -2,7 +2,10 @@ package de.swe.oo.server.cards;
 
 import de.swe.oo.server.game.Game;
 import de.swe.oo.server.game.LoveLetterGame;
+import de.swe.oo.server.messages.GameChoiceRequestMessage;
 import de.swe.oo.server.player.Player;
+
+import static java.lang.Integer.parseInt;
 
 public class Guard extends Card{
     private static String NAME = "Guard";
@@ -13,7 +16,27 @@ public class Guard extends Card{
     }
 
     @Override
+    /**When you discard the Guard, choose a player and name a number (other than 1).
+     * If that player has that number in their hand, that player is knocked out of the round.
+     */
     void discard(){
+        String [] namelist = currentGame.getNameOfActivePlayers().toArray(new String[0]);
+        /** Player can choose a name */
+        owner.sendMessage(new GameChoiceRequestMessage("Choose one of the names", namelist));
+        Player targetPlayer= currentGame.getPlayer(owner.getLastResponse());
+        /** Choose number value of cards*/
+        String[] options = {"2", "3", "4", "5", "6", "7", "8"};
+        owner.sendMessage(new GameChoiceRequestMessage("Choose number", options));
+        int responseIndex = parseInt(owner.getLastResponse().trim());
+        int response = parseInt(options[responseIndex]);
+        Card targetPlayerCardOne= targetPlayer.getHands(0);
+        Card targetPlayerCardTwo= targetPlayer.getHands(1);
+
+        /**If number is consistent with value of one of the cards eliminate player*/
+
+        if(response == targetPlayerCardOne.value || response == targetPlayerCardTwo.value){
+            currentGame.playerKickedOff(targetPlayer);
+        }
 
     }
 }
