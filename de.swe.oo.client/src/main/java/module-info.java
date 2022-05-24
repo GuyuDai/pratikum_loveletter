89ec8ebbe5a545d2431796b3701ebdcf466460fa
Nik <nissln@gitlab2.cip.ifi.lmu.de>
2022-05-24 21:35:46 +0200

    jar

--- de.swe.oo.client/src/main/java/module-info.java
+++ de.swe.oo.client/src/main/java/module-info.java
@@ -1,10 +1,12 @@
 module de.swe.oo.client {
-    exports de.swe.oo.client.debugClient;
+    exports de.swe.oo.client.guiclient;
     exports de.swe.oo.client.connection;
     exports de.swe.oo.client;
 
     requires javafx.controls;
     requires javafx.graphics;
     requires javafx.fxml;
+    requires javafx.base;
+
   opens de.swe.oo.client.guiclient;
 }
